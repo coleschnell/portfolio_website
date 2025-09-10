c5db00859efb0df74f76a2e4d26772e36fb9596c
coleschnell <coleschnell@gmail.com>
2025-09-10 17:08:00 -0500

    add memphis unemployment graphs

--- src/routes/memphis-economy-dashboard/+page.svelte
+++ src/routes/memphis-economy-dashboard/+page.svelte
@@ -42,92 +42,138 @@
     <h3>Components of Memphis' Index of Consumer Sentiment</h3>
 
     <div class="flex-rows-2">
-    <div
-        style="min-height:418px; width:480px !important;"
-        id="datawrapper-vis-q5Gqz"
-    >
-        <script
-            type="text/javascript"
-            defer 
-            src="https://datawrapper.dwcdn.net/q5Gqz/embed.js"
-            charset="utf-8"
-            data-target="#datawrapper-vis-q5Gqz"
-        ></script><noscript
-            ><img
-                src="https://datawrapper.dwcdn.net/q5Gqz/full.png"
-                alt="Personal Finances Current Conditions (Line chart)"
-            /></noscript
+        <div
+            style="min-height:418px; width:480px !important;"
+            id="datawrapper-vis-q5Gqz"
         >
-    </div>
+            <script
+                type="text/javascript"
+                defer
+                src="https://datawrapper.dwcdn.net/q5Gqz/embed.js"
+                charset="utf-8"
+                data-target="#datawrapper-vis-q5Gqz"
+            ></script><noscript
+                ><img
+                    src="https://datawrapper.dwcdn.net/q5Gqz/full.png"
+                    alt="Personal Finances Current Conditions (Line chart)"
+                /></noscript
+            >
+        </div>
 
+        <div
+            style="min-height:418px; width:480px !important;"
+            id="datawrapper-vis-ivLfz"
+        >
+            <script
+                type="text/javascript"
+                defer
+                src="https://datawrapper.dwcdn.net/ivLfz/embed.js"
+                charset="utf-8"
+                data-target="#datawrapper-vis-ivLfz"
+            ></script><noscript
+                ><img
+                    src="https://datawrapper.dwcdn.net/ivLfz/full.png"
+                    alt="Personal Finances 12-Month Expectations (Line chart)"
+                /></noscript
+            >
+        </div>
+        <div
+            style="min-height:424px; width:480px !important;"
+            id="datawrapper-vis-XdfY8"
+        >
+            <script
+                type="text/javascript"
+                defer
+                src="https://datawrapper.dwcdn.net/XdfY8/embed.js"
+                charset="utf-8"
+                data-target="#datawrapper-vis-XdfY8"
+            ></script><noscript
+                ><img
+                    src="https://datawrapper.dwcdn.net/XdfY8/full.png"
+                    alt="Business Conditions 12-Month Expectations (Line chart)"
+                /></noscript
+            >
+        </div>
+        <div
+            style="min-height:418px; width:480px !important;"
+            id="datawrapper-vis-75Cua"
+        >
+            <script
+                type="text/javascript"
+                defer
+                src="https://datawrapper.dwcdn.net/75Cua/embed.js"
+                charset="utf-8"
+                data-target="#datawrapper-vis-75Cua"
+            ></script><noscript
+                ><img
+                    src="https://datawrapper.dwcdn.net/75Cua/full.png"
+                    alt="Business Conditions 5-Year Expectations (Line chart)"
+                /></noscript
+            >
+        </div>
+        <div
+            style="min-height:418px; width:480px !important;"
+            id="datawrapper-vis-EvNVX"
+        >
+            <script
+                type="text/javascript"
+                defer
+                src="https://datawrapper.dwcdn.net/EvNVX/embed.js"
+                charset="utf-8"
+                data-target="#datawrapper-vis-EvNVX"
+            ></script><noscript
+                ><img
+                    src="https://datawrapper.dwcdn.net/EvNVX/full.png"
+                    alt="Buying Conditions (Line chart)"
+                /></noscript
+            >
+        </div>
+    </div>
+    <h3>Memphis employment stituation</h3>
     <div
-        style="min-height:418px; width:480px !important;"
-        id="datawrapper-vis-ivLfz"
+        style="min-height:429px; width:990px !important;"
+        id="datawrapper-vis-YszlU"
     >
         <script
             type="text/javascript"
             defer
-            src="https://datawrapper.dwcdn.net/ivLfz/embed.js"
+            src="https://datawrapper.dwcdn.net/YszlU/embed.js"
             charset="utf-8"
-            data-target="#datawrapper-vis-ivLfz"
+            data-target="#datawrapper-vis-YszlU"
         ></script><noscript
             ><img
-                src="https://datawrapper.dwcdn.net/ivLfz/full.png"
-                alt="Personal Finances 12-Month Expectations (Line chart)"
+                src="https://datawrapper.dwcdn.net/YszlU/full.png"
+                alt="Memphis unemployment rate, seasonally adjusted (Line chart)"
             /></noscript
         >
     </div>
-    <div
-        style="min-height:424px; width:480px !important;"
-        id="datawrapper-vis-XdfY8"
-    >
+    <div style="min-height:455px;width:990px !important;" id="datawrapper-vis-0gSB1">
         <script
             type="text/javascript"
             defer
-            src="https://datawrapper.dwcdn.net/XdfY8/embed.js"
+            src="https://datawrapper.dwcdn.net/0gSB1/embed.js"
             charset="utf-8"
-            data-target="#datawrapper-vis-XdfY8"
+            data-target="#datawrapper-vis-0gSB1"
         ></script><noscript
             ><img
-                src="https://datawrapper.dwcdn.net/XdfY8/full.png"
-                alt="Business Conditions 12-Month Expectations (Line chart)"
+                src="https://datawrapper.dwcdn.net/0gSB1/full.png"
+                alt="Memphis' labor force vs employment, seasonally adjusted (Line chart)"
             /></noscript
         >
     </div>
-    <div
-        style="min-height:418px; width:480px !important;"
-        id="datawrapper-vis-75Cua"
-    >
+    <div style="min-height:428px;width:990px !important;" id="datawrapper-vis-PcDvh">
         <script
             type="text/javascript"
             defer
-            src="https://datawrapper.dwcdn.net/75Cua/embed.js"
+            src="https://datawrapper.dwcdn.net/PcDvh/embed.js"
             charset="utf-8"
-            data-target="#datawrapper-vis-75Cua"
+            data-target="#datawrapper-vis-PcDvh"
         ></script><noscript
             ><img
-                src="https://datawrapper.dwcdn.net/75Cua/full.png"
-                alt="Business Conditions 5-Year Expectations (Line chart)"
+                src="https://datawrapper.dwcdn.net/PcDvh/full.png"
+                alt="Number of people unemployment in Memphis, seasonally adjusted (Line chart)"
             /></noscript
         >
-    </div>
-    <div
-        style="min-height:418px; width:480px !important;"
-        id="datawrapper-vis-EvNVX"
-    >
-        <script
-            type="text/javascript"
-            defer
-            src="https://datawrapper.dwcdn.net/EvNVX/embed.js"
-            charset="utf-8"
-            data-target="#datawrapper-vis-EvNVX"
-        ></script><noscript
-            ><img
-                src="https://datawrapper.dwcdn.net/EvNVX/full.png"
-                alt="Buying Conditions (Line chart)"
-            /></noscript
-        >
-    </div>
     </div>
 </article>
 
@@ -142,11 +188,14 @@
         margin: 100px 0 100px 0;
     }
 
-    h1, h2, h3, p {
+    h1,
+    h2,
+    h3,
+    p {
         width: 720px;
     }
 
-    .flex-rows-2{
+    .flex-rows-2 {
         width: 990px;
         display: flex;
         flex-direction: row;
@@ -154,6 +203,4 @@
         justify-content: space-between;
         gap: 20px;
     }
-
-
 </style>
