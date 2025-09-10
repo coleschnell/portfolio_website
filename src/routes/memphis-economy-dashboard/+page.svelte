5a1630bef254e3852b058ebd06d4e1a79cace849
coleschnell <coleschnell@gmail.com>
2025-09-10 15:42:24 -0500

    add memphis economic dashboard

---
+++ src/routes/memphis-economy-dashboard/+page.svelte
@@ -0,0 +1,159 @@
+<head><meta name="robots" content="noindex" /></head>
+
+<article>
+    <h1>Memphis economic indicators</h1>
+    <div
+        style="min-height:418px; width:990px !important;"
+        id="datawrapper-vis-jLeTW"
+    >
+        <script
+            type="text/javascript"
+            defer
+            src="https://datawrapper.dwcdn.net/jLeTW/embed.js"
+            charset="utf-8"
+            data-target="#datawrapper-vis-jLeTW"
+        ></script>
+        <noscript
+            ><img
+                src="https://datawrapper.dwcdn.net/jLeTW/full.png"
+                alt="Sales tax revenue year-over-year for the largest Tennessee counties (Line chart)"
+            />
+        </noscript>
+    </div>
+
+    <div
+        style="min-height:330px; width:990px !important;"
+        id="datawrapper-vis-3J6xt"
+    >
+        <script
+            type="text/javascript"
+            defer
+            src="https://datawrapper.dwcdn.net/3J6xt/embed.js"
+            charset="utf-8"
+            data-target="#datawrapper-vis-3J6xt"
+        ></script><noscript
+            ><img
+                src="https://datawrapper.dwcdn.net/3J6xt/full.png"
+                alt="Memphis' Index of Consumer Sentiment (ICS) (Line chart)"
+            /></noscript
+        >
+    </div>
+
+    <h3>Components of Memphis' Index of Consumer Sentiment</h3>
+
+    <div class="flex-rows-2">
+    <div
+        style="min-height:418px; width:480px !important;"
+        id="datawrapper-vis-q5Gqz"
+    >
+        <script
+            type="text/javascript"
+            defer 
+            src="https://datawrapper.dwcdn.net/q5Gqz/embed.js"
+            charset="utf-8"
+            data-target="#datawrapper-vis-q5Gqz"
+        ></script><noscript
+            ><img
+                src="https://datawrapper.dwcdn.net/q5Gqz/full.png"
+                alt="Personal Finances Current Conditions (Line chart)"
+            /></noscript
+        >
+    </div>
+
+    <div
+        style="min-height:418px; width:480px !important;"
+        id="datawrapper-vis-ivLfz"
+    >
+        <script
+            type="text/javascript"
+            defer
+            src="https://datawrapper.dwcdn.net/ivLfz/embed.js"
+            charset="utf-8"
+            data-target="#datawrapper-vis-ivLfz"
+        ></script><noscript
+            ><img
+                src="https://datawrapper.dwcdn.net/ivLfz/full.png"
+                alt="Personal Finances 12-Month Expectations (Line chart)"
+            /></noscript
+        >
+    </div>
+    <div
+        style="min-height:424px; width:480px !important;"
+        id="datawrapper-vis-XdfY8"
+    >
+        <script
+            type="text/javascript"
+            defer
+            src="https://datawrapper.dwcdn.net/XdfY8/embed.js"
+            charset="utf-8"
+            data-target="#datawrapper-vis-XdfY8"
+        ></script><noscript
+            ><img
+                src="https://datawrapper.dwcdn.net/XdfY8/full.png"
+                alt="Business Conditions 12-Month Expectations (Line chart)"
+            /></noscript
+        >
+    </div>
+    <div
+        style="min-height:418px; width:480px !important;"
+        id="datawrapper-vis-75Cua"
+    >
+        <script
+            type="text/javascript"
+            defer
+            src="https://datawrapper.dwcdn.net/75Cua/embed.js"
+            charset="utf-8"
+            data-target="#datawrapper-vis-75Cua"
+        ></script><noscript
+            ><img
+                src="https://datawrapper.dwcdn.net/75Cua/full.png"
+                alt="Business Conditions 5-Year Expectations (Line chart)"
+            /></noscript
+        >
+    </div>
+    <div
+        style="min-height:418px; width:480px !important;"
+        id="datawrapper-vis-EvNVX"
+    >
+        <script
+            type="text/javascript"
+            defer
+            src="https://datawrapper.dwcdn.net/EvNVX/embed.js"
+            charset="utf-8"
+            data-target="#datawrapper-vis-EvNVX"
+        ></script><noscript
+            ><img
+                src="https://datawrapper.dwcdn.net/EvNVX/full.png"
+                alt="Buying Conditions (Line chart)"
+            /></noscript
+        >
+    </div>
+    </div>
+</article>
+
+<style>
+    article {
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        margin: 100px 0 100px 0;
+    }
+
+    h1, h2, h3, p {
+        width: 720px;
+    }
+
+    .flex-rows-2{
+        width: 990px;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        gap: 20px;
+    }
+
+
+</style>
