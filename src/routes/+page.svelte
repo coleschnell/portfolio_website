695c589dd6bdf64ac2ab6ae0997b7eab0dde450b
coleschnell <coleschnell@gmail.com>
2026-09-22 21:53:48 -0500

    fix alignment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,17 @@
 <article>
-<p>Hello! I'm Cole Schnell. I'm a computational journalist currently working as a data reporter for American City Business Journals.</p>
-<p>Contact me at <a href="mailto:hey@coles.dev">hey@coles.dev</a>.</p>
+    <p>
+        Hello! I'm Cole Schnell. I'm a computational journalist currently
+        working as a data reporter for American City Business Journals.
+    </p>
+    <p>Contact me at <a href="mailto:hey@coles.dev">hey@coles.dev</a>.</p>
 </article>
 
 <style>
-
-article{
-    display: flex;
-    flex-direction: column;
-    gap: 30px;
-    align-items: center;
-    justify-content: center;
-    height: 85vh;
-}
-
-article > p {
-    width: 600px;
-}
-
-
+    article {
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+        justify-content: center;
+        margin: 20px;
+    }
 </style>
